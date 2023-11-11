f89b6f2d78e1ea04ad6b0700d5206cfc9bd6f713
Asem Umarkulova <umarkulovaasem@gmail.com>
2023-11-11 12:48:27 +0500

    done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [data,setData] = useState();
+  const [name,setName] = useState();
+  const [weight,setWeight] = useState();
+  const [number,setNumber] = useState(3); 
+
+  useEffect(() => {
+    axios
+      .get(`https://pokeapi.co/api/v2/pokemon/${number}`)
+      .then((response) => {
+        setData(response.data);
+        setName(response.data.name)
+        setWeight(response.data.weight)
+      })
+      .catch((err) => {
+      window.alert(err);
+     });
+  },[number]);
+
+  if(!data) {
+    return null;
+  }
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Pokemon</h1>
+      <input type={"number"} onChange={(e)=>{setNumber(e.target.value)}}/>
+      <button>Show</button>
+      <h2>Name : {name}</h2>
+      <h3>Weight : {weight}</h3>
+      <img 
+        src={data?.sprites?.other?.dream_world?.front_default
+      }
+      />
+      <p>My abilities are:</p>    
+      {data 
+        ? data?.abilities?.map((value,key) => {
+          return <div key={key}>{value.ability.name}</div>;
+        })
+      :""}
     </div>
   );
 }
